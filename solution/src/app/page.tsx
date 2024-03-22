caa81f82379adc36a40d889ff98acdfab53a07d2
Daniel Westerlund <daniel.westerlund@findwise.com>
2024-03-22 22:07:41 +0100

    Initial commit

---
+++ solution/src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main className="px-4">
+      <div className="bg-white rounded-lg px-5 pb-5 pt-6">
+        <h1 className="text-[#02295a] text-3xl font-bold">Personal info</h1>
+        <p className="text-[#9699ab]">
+          Please provide your name, email adress, and phone number.
+        </p>
+        <form>
+          <p>form stuff</p>
+        </form>
+      </div>
+    </main>
+  );
+}
