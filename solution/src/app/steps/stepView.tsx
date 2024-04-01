ee0ef7c3d40f799e8fd0b8f140cde08a59afb6d6
Daniel Westerlund <westerlund.da@gmail.com>
2024-04-01 17:42:58 +0200

    Step view responsive

--- solution/src/app/steps/stepView.tsx
+++ solution/src/app/steps/stepView.tsx
@@ -17,18 +17,26 @@ export default function StepView({
         className="hidden sm:inline object-cover rounded-2xl"
       />
       {([1, 2, 3, 4] as Step[]).map((num) => (
-        <button
-          key={num}
-          onClick={() => setStep(num)}
-          className={`h-8 w-8 flex items-center font-bold justify-center mx-2 rounded-full border-white border-[1px] z-10 ${
-            num === step || (num === 4 && step === 5)
-              ? "bg-lightBlue text-marineBlue"
-              : "text-white"
-          }`}
-        >
-          {num}
-        </button>
+        <div className="z-10 flex my-3">
+          <button
+            key={num}
+            onClick={() => setStep(num)}
+            className={`h-8 w-8 sm:h-9 sm:w-9 flex items-center font-bold justify-center mx-2 rounded-full border-white border-[1px] ${
+              num === step || (num === 4 && step === 5)
+                ? "bg-lightBlue text-marineBlue"
+                : "text-white"
+            }`}
+          >
+            {num}
+          </button>
+          <div className="hidden sm:block ml-2">
+            <p className="text-xs text-coolGray">{`STEP ${num}`}</p>
+            <p className="text-sm text-white font-bold">{stepData[num - 1]}</p>
+          </div>
+        </div>
       ))}
     </div>
   );
 }
+
+const stepData = ["YOUR INFO", "SELECT PLAN", "ADD-ONS", "SUMMARY"];
